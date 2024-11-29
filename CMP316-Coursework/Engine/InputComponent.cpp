a751039e50de7f2ee327ae83beed972ea8b3fb64
2200147 <2200147@uad.ac.uk>
2024-11-29 16:07:40 +0000

    making components

---
+++ CMP316-Coursework/Engine/InputComponent.cpp
@@ -0,0 +1,7 @@
+#include "pch.h"
+#include "InputComponent.h"
+
+InputComponent::InputComponent()
+{
+	controlls = { { -1 }, { -1 }, { -1 }, { -1 } };
+}
---
+++ CMP316-Coursework/Engine/InputComponent.h
@@ -0,0 +1,8 @@
+#pragma once
+class InputComponent
+{
+	InputComponent();
+private:
+	std::vector<int> controlls;
+};
+
